a7e01db5a713f647686da2e416fdb968f74782f7
geek <elmainiaymane03@gmail.com>
2024-09-14 22:24:06 +0100

    add input validations using strategy pattern and functional programming style, and refactor the input scanners

---
+++ src/main/java/com/wora/client/infrastructure/presentation/ClientUi.java
@@ -0,0 +1,15 @@
+package com.wora.client.infrastructure.presentation;
+
+import com.wora.client.application.services.ClientService;
+
+public class ClientUi {
+    private final ClientService service;
+
+    public ClientUi(ClientService service) {
+        this.service = service;
+    }
+
+    public void create() {
+
+    }
+}
---
+++ src/main/java/com/wora/common/utils/ValidationStrategies.java
@@ -0,0 +1,31 @@
+package com.wora.common.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class ValidationStrategies {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+    );
+
+    public static final Predicate<String> NOT_BLANK = s -> !s.isEmpty();
+    public static final Predicate<Integer> POSITIVE_INT = i -> i > 0;
+    public static final Predicate<Double> POSITIVE_DOUBLE = i -> i > 0;
+    public static final Predicate<Float> POSITIVE_FLOAT = i -> i > 0;
+    public static final Predicate<LocalDate> FUTURE_DATE = d -> d.isAfter(LocalDate.now());
+    public static final Predicate<LocalDateTime> FUTURE_DATE_TIME = d -> d.isAfter(LocalDateTime.now());
+    public static final Predicate<String> VALID_EMAIL = email -> EMAIL_PATTERN.matcher(email).matches();
+
+    @SafeVarargs
+    public static <T> Predicate<T> combine(Predicate<T>... strategies) {
+        return input -> {
+            for (Predicate<T> strategy : strategies) {
+                if (!strategy.test(input)) return false;
+            }
+            return true;
+        };
+    }
+}
