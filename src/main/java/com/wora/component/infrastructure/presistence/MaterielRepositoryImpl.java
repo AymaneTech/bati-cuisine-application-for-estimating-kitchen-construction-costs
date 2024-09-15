15bdf1da868e14bd67aaa7a2376d1e5ff5efcbd7
geek <elmainiaymane03@gmail.com>
2024-09-15 16:29:27 +0100

    add: create materiel repository implemetation c

--- src/main/java/com/wora/component/infrastructure/presistence/MaterielRepositoryImpl.java
+++ src/main/java/com/wora/component/infrastructure/presistence/MaterielRepositoryImpl.java
@@ -1,4 +1,43 @@
 package com.wora.component.infrastructure.presistence;
 
-public class MaterielRepositoryImpl {
+import com.wora.common.infrastructure.persistence.BaseRepository;
+import com.wora.common.infrastructure.persistence.BaseRepositoryImpl;
+import com.wora.component.domain.entity.Materiel;
+import com.wora.component.domain.enums.ComponentType;
+import com.wora.component.domain.exception.ComponentNotFoundException;
+import com.wora.component.infrastructure.mapper.MaterielResultSetMapper;
+
+import java.util.UUID;
+
+import static com.wora.common.util.QueryExecutor.execute;
+
+
+public class MaterielRepositoryImpl extends BaseRepositoryImpl<Materiel, UUID> implements BaseRepository<Materiel, UUID> {
+    public MaterielRepositoryImpl(MaterielResultSetMapper mapper) {
+        super("materiels", mapper);
+    }
+
+    @Override
+    public Materiel create(Materiel materiel) {
+        final String query = """
+                INSERT INTO materiels (name, tva, component_type, unit_cost, quantity, transport_cost, quantity_coefficient, project_id, id)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                """;
+        execute(query, stmt -> mapper.map(materiel, stmt));
+        return findById(materiel.id().value())
+                .orElseThrow(() -> new ComponentNotFoundException(materiel.id().value(), ComponentType.MATERIEL));
+    }
+
+    @Override
+    public Materiel update(UUID uuid, Materiel materiel) {
+        final String query = """
+                UPDATE materiels
+                SET name = ?, tva = ?, component_type = ?::component_type, unit_cost = ?, quantity = ?,
+                transport_cost = ?, quantity_coefficient = ?, project_id = ?
+                WHERE id = ?
+                """;
+        execute(query, stmt -> mapper.map(materiel, stmt));
+        return findById(materiel.id().value())
+                .orElseThrow(() -> new ComponentNotFoundException(materiel.id().value(), ComponentType.MATERIEL));
+    }
 }
