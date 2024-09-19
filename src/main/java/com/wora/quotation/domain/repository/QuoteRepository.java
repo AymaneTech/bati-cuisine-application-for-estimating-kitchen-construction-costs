a61d2780aaa1567ec8ea4629479c0d96976a6f60
geek <elmainiaymane03@gmail.com>
2024-09-19 17:20:47 +0100

    add implementation of find by proejct id and client id in repository of quote

--- src/main/java/com/wora/quotation/domain/repository/QuoteRepository.java
+++ src/main/java/com/wora/quotation/domain/repository/QuoteRepository.java
@@ -3,8 +3,12 @@ package com.wora.quotation.domain.repository;
 import com.wora.common.infrastructure.persistence.BaseRepository;
 import com.wora.quotation.domain.Quote;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface QuoteRepository extends BaseRepository<Quote, UUID> {
 
+    List<Quote> findAllByProjectId(UUID projectId);
+
+    List<Quote> findAllByClientId(UUID clientId);
 }
--- src/main/java/com/wora/quotation/infrastructure/presistence/QuoteRepositoryImpl.java
+++ src/main/java/com/wora/quotation/infrastructure/presistence/QuoteRepositoryImpl.java
@@ -6,8 +6,12 @@ import com.wora.quotation.domain.Quote;
 import com.wora.quotation.domain.exception.QuoteNotFoundException;
 import com.wora.quotation.domain.repository.QuoteRepository;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import static com.wora.common.util.QueryExecutor.executeQueryWithPreparedStatement;
 import static com.wora.common.util.QueryExecutor.executeWithSingleUpdate;
 
 public class QuoteRepositoryImpl extends BaseRepositoryImpl<Quote, UUID> implements QuoteRepository {
@@ -39,4 +43,43 @@ public class QuoteRepositoryImpl extends BaseRepositoryImpl<Quote, UUID> impleme
         return findById(id)
                 .orElseThrow(() -> new QuoteNotFoundException(id));
     }
+
+    @Override
+    public List<Quote> findAllByProjectId(UUID projectId) {
+        final List<Quote> quotes = new ArrayList<>();
+        final String query = """
+                SELECT * FROM quotes
+                WHERE project_id = ?
+                AND WHERE deleted_at IS NULL
+                """;
+        executeQueryWithPreparedStatement(query, stmt -> {
+            stmt.setObject(1, projectId);
+            final ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                quotes.add(mapper.map(rs));
+            }
+        });
+        return quotes;
+    }
+
+    @Override
+    public List<Quote> findAllByClientId(UUID clientId) {
+        final List<Quote> quotes = new ArrayList<>();
+        final String query = """
+                SELECT q.*
+                FROM quotes q
+                JOIN projects p ON q.project_id = p.id
+                WHERE p.client_id = ?
+                AND q.deleted_at IS NULL
+                """;
+        executeQueryWithPreparedStatement(query, stmt -> {
+            stmt.setObject(1, clientId);
+            final ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                quotes.add(mapper.map(rs));
+            }
+        });
+        return quotes;
+    }
+
 }
