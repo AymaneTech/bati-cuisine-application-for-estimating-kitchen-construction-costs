a7a1bbbc453afe139015d9d2735e726b8f157244
geek <elmainiaymane03@gmail.com>
2024-09-19 11:19:09 +0100

    add quote repository implementation and also entity <-> result set mapper'

--- src/main/java/com/wora/quotation/infrastructure/mapper/QuoteResultSetMapper.java
+++ src/main/java/com/wora/quotation/infrastructure/mapper/QuoteResultSetMapper.java
@@ -1,4 +1,37 @@
 package com.wora.quotation.infrastructure.mapper;
 
-public class QuoteResultSetMapper {
+import com.wora.common.infrastructure.mapper.BaseEntityResultSetMapper;
+import com.wora.project.domain.entity.Project;
+import com.wora.quotation.domain.Quote;
+import com.wora.quotation.domain.valueObject.QuoteId;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class QuoteResultSetMapper implements BaseEntityResultSetMapper<Quote> {
+    @Override
+    public Quote map(ResultSet rs) throws SQLException {
+        return new Quote(
+                new QuoteId(UUID.fromString(rs.getString("id"))),
+                getDate("issue_date", rs),
+                getDate("validity_date", rs),
+                rs.getBoolean("accepted"),
+                new Project(),
+                getDateTime("created_at", rs),
+                getDateTime("updated_at", rs)
+        );
+    }
+
+    @Override
+    public void map(Quote quote, PreparedStatement stmt) throws SQLException {
+        int count = 1;
+        stmt.setDate(count++, Date.valueOf(quote.issueDate()));
+        stmt.setDate(count++, Date.valueOf(quote.validityDate()));
+        stmt.setBoolean(count++, quote.accepted());
+        stmt.setObject(count++, quote.project().id().value());
+        stmt.setObject(count++, quote.id().value());
+    }
 }
--- src/main/java/com/wora/quotation/infrastructure/presistence/QuoteRepositoryImpl.java
+++ src/main/java/com/wora/quotation/infrastructure/presistence/QuoteRepositoryImpl.java
@@ -1,4 +1,42 @@
 package com.wora.quotation.infrastructure.presistence;
 
-public class QuoteRepositoryImpl {
+import com.wora.common.infrastructure.mapper.BaseEntityResultSetMapper;
+import com.wora.common.infrastructure.persistence.BaseRepositoryImpl;
+import com.wora.quotation.domain.Quote;
+import com.wora.quotation.domain.exception.QuoteNotFoundException;
+import com.wora.quotation.domain.repository.QuoteRepository;
+
+import java.util.UUID;
+
+import static com.wora.common.util.QueryExecutor.executeWithSingleUpdate;
+
+public class QuoteRepositoryImpl extends BaseRepositoryImpl<Quote, UUID> implements QuoteRepository {
+
+    public QuoteRepositoryImpl(BaseEntityResultSetMapper<Quote> mapper) {
+        super("quotes", mapper);
+    }
+
+    @Override
+    public Quote create(Quote quote) {
+        final String query = """
+                INSERT INTO quotes (issue_date, validity_date, accepted, project_id, id)
+                VALUES (?,?,?,?,?)
+                """;
+        executeWithSingleUpdate(query, stmt -> mapper.map(quote, stmt));
+        return findById(quote.id().value())
+                .orElseThrow(() -> new QuoteNotFoundException(quote.id().value()));
+
+    }
+
+    @Override
+    public Quote update(UUID id, Quote quote) {
+        final String query = """
+                UPDATE quotes
+                SET issue_date = ?, validity_date = ?, accepted = ?, project_id = ?
+                WHERE id = ?
+                """;
+        executeWithSingleUpdate(query, stmt -> mapper.map(quote, stmt));
+        return findById(id)
+                .orElseThrow(() -> new QuoteNotFoundException(id));
+    }
 }
